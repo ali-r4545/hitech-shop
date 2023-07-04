fe5228250f86bb49cf636ed35d6b1b6420f3b681
Ali <Alirasteh17@gmail.com>
2023-07-04 11:02:07 +0330

    explore button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Header.css";
+const [show, setShow] = useState(false);
+const explore = useRef();
 
 const Header = () => {
+  const switchtf = () => {
+    setShow((current) => !current);
+  };
+  useEffect(() => {
+    window.scrollTo({
+      top: explore?.current?.offsetTop,
+      behavior: "smooth",
+    });
+  }, [show]);
   return (
     <div>
       <header className="header">
@@ -9,7 +20,7 @@ const Header = () => {
           <h1>welcome to tech shop</h1>
           <h2>you can find many products here</h2>
           <h3>lets have a look for more</h3>
-          <a className="banner-btn" href="#explore">
+          <a className="banner-btn" href="#explore" onClick={switchtf}>
             Explore
           </a>
         </div>
